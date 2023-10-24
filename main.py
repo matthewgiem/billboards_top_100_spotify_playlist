6a046bd7dd9aa7b2d101c5fb29c564caea26a561
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-10-24 13:01:11 +0700

    prompt user for a date, them print to the screen the billboards top 100 for that date

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+date = input("what year-month-day do you want the billboard top 100 for? ")
+
+
+URL = f"https://www.billboard.com/charts/hot-100/{date}/"
+
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, "html.parser")
+
+top100 = soup.find_all("div", {"class": "o-chart-results-list-row-container"})
+
+
+for song in top100:
+    print(song.h3.text.strip())
